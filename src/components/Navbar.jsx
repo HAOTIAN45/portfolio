975243e94f1a059339ab2795fc92d3ab2cdc9d81
HAOTIAN45 <danidwinugroho@gmail.com>
2025-03-21 09:15:25 +0700

    Change text in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
 import { Link } from 'react-scroll'
 import { motion } from 'framer-motion'
+import GradientText from "../components/GradientText.jsx"
 
 const Navbar = () => {
     const [nav, setNav] = useState(false)
@@ -18,56 +19,119 @@ const Navbar = () => {
         open: {
             x: 0,
             transition: {
-            stiffness: 20,
-            damping: 15
+                stiffness: 20,
+                damping: 15
             }
         },
         closed: {
             x: '-100%',
             transition: {
-            stiffness: 20,
-            damping: 15
+                stiffness: 20,
+                damping: 15
             }
         }
     }
 
-  return (
-    <div className='fixed top-0 left-0 w-full bg-opacity-70 backdrop-blur-md z-50'>
-        <div className='max-w-[1300px] mx-auto  flex justify-between text-gray-200
+    return (
+        <div className='fixed top-0 left-0 w-full bg-opacity-70 backdrop-blur-md z-50'>
+            <div className='max-w-[1300px] mx-auto  flex justify-between text-gray-200
         text-xl items-center px-12 h-20'>
 
-            <a href="#">H A O</a>
+                <a href="/about">
+                    <GradientText
+                        colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
+                        animationSpeed={3}
+                        showBorder={false}
+                        className="custom-class"
+                    >
+                        H A O
+                    </GradientText>
+                </a>
 
-            <ul className='hidden md:flex gap-12 z-10 cursor-pointer'>
-                <li><Link to="skills" smooth={true} offset={50} duration={500}>About</Link></li>
-                <li><Link to="portfolio" smooth={true} offset={50} duration={500}>Portfolio</Link></li>
-                <li><Link to="contact" smooth={true} offset={50} duration={500}>Contact</Link></li>
-            </ul>
-
-            <div onClick={toggleNav} className='md:hidden z-50 text-gray-200'>
-                {nav ? <AiOutlineClose size={30} /> : <AiOutlineMenu size={30} />}
-            </div>
-
-            <motion.div
-            initial={false}
-            animate={nav ? 'open' : 'closed'}
-            variants={menuVariants}
-            className='fixed left-0 top-0 w-full min-h-screen bg-gray-900 z-40'
-            >
-                <ul className='font-semibold text-4xl space-y-8 mt-24 text-center'>
-                    <li><Link to="skills" onClick={closeNav} smooth={true} offset={50} duration={500}>About</Link></li>
-                    <li><Link to="portfolio" onClick={closeNav} smooth={true} offset={50} duration={500}>Portfolio</Link></li>
-                    <li><Link to="contact" onClick={closeNav} smooth={true} offset={50} duration={500}>Contact</Link></li>
+                <ul className='hidden md:flex gap-12 z-10 cursor-pointer'>
+                    <li><Link to="skills" smooth={true} offset={50} duration={500}>
+                        <GradientText
+                            colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
+                            animationSpeed={3}
+                            showBorder={false}
+                            className="custom-class"
+                        >
+                            About
+                        </GradientText>
+                    </Link></li>
+                    <li><Link to="portfolio" smooth={true} offset={50} duration={500}>
+                        <GradientText
+                            colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
+                            animationSpeed={3}
+                            showBorder={false}
+                            className="custom-class"
+                        >
+                            Portfolio
+                        </GradientText>
+                    </Link></li>
+                    <li><Link to="contact" smooth={true} offset={50} duration={500}>
+                        <GradientText
+                            colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
+                            animationSpeed={3}
+                            showBorder={false}
+                            className="custom-class"
+                        >
+                            Contact
+                        </GradientText>
+                    </Link></li>
                 </ul>
 
+                <div onClick={toggleNav} className='md:hidden z-50 text-gray-200'>
+                    {nav ? <AiOutlineClose size={30} /> : <AiOutlineMenu size={30} />}
+                </div>
 
-            </motion.div>
+                <motion.div
+                    initial={false}
+                    animate={nav ? 'open' : 'closed'}
+                    variants={menuVariants}
+                    className='fixed left-0 top-0 w-full min-h-screen bg-gray-900 z-40'
+                >
+                    <ul className='font-semibold text-4xl space-y-8 mt-24 text-center'>
+                        <li><Link to="skills" onClick={closeNav} smooth={true} offset={50} duration={500}>
+                            <GradientText
+                                colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
+                                animationSpeed={3}
+                                showBorder={false}
+                                className="custom-class"
+                            >
+                                About
+                            </GradientText>
+                        </Link></li>
+                        <li><Link to="portfolio" onClick={closeNav} smooth={true} offset={50} duration={500}>
+                            <GradientText
+                                colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
+                                animationSpeed={3}
+                                showBorder={false}
+                                className="custom-class"
+                            >
+                                Portfolio
+                            </GradientText>
+                        </Link></li>
+                        <li><Link to="contact" onClick={closeNav} smooth={true} offset={50} duration={500}>
+                            <GradientText
+                                colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
+                                animationSpeed={3}
+                                showBorder={false}
+                                className="custom-class"
+                            >
+                                Contact
+                            </GradientText>
+                        </Link></li>
+                    </ul>
+
+
+                </motion.div>
 
 
 
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
